e62a3500c8943740317f414f58396d9940b1bb21
Suleman Ali <salmanalishah825@gmail.com>
2026-04-09 01:27:58 +0500

    Fisrt Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.gradle.accessors.dm.LibrariesForLibs
+
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.google.firebase.firebase.perf)
+    alias(libs.plugins.google.gms.google.services)
+}
+
+android {
+    namespace = "com.example.socialconnect"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.socialconnect"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.perf)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
