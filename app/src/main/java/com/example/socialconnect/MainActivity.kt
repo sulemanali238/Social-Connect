333f196bde8365df9d26c3386098c6c425b83a58
Suleman Ali <salmanalishah825@gmail.com>
2026-04-10 12:41:01 +0500

    2nd Commit

--- app/src/main/java/com/example/socialconnect/MainActivity.kt
+++ app/src/main/java/com/example/socialconnect/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.socialconnect
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class  MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Get Started → goes to SignUpActivity
+        findViewById<MaterialButton>(R.id.btnGetStarted).setOnClickListener {
+            startActivity(Intent(this, activity_SignUp::class.java))
+        }
+
+        // Log In → goes to LoginActivity
+        findViewById<TextView>(R.id.btnGoToLogin).setOnClickListener {
+            startActivity(Intent(this, activity_Login::class.java))
         }
     }
 }
